bd79a32517b53440c0c74e72afff09a110183633
Luiz Felipe Santana Dos Santos <luiz.santos811@etec.sp.gov.br>
2022-10-15 11:45:37 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ export function App() {
   return (
     <>
       <h1 className='font-bold text-2xl text-gray-800 text-center'>
-        Unidos do Morro do Acai I
+        Ignite Lab
       </h1>
       <button className='bg-cyan-500 font-medium px-4 py-2 rounded-sm text-white hover:bg-cyan-300'>
-        Enviar
+        Enviar 
         </button>
     </>
   )
